99884ad6c12a93824fdae16774140d59413c52ea
Jacob Newfield <j.newfield@f5.com>
2018-10-02 11:56:04 -0700

    Implemented: help info; clear logs directive; quit function

--- runscripts.py
+++ runscripts.py
@@ -3,15 +3,36 @@
 import subprocess
 import getpass
 import time
+import sys
 
 # Variables
-NUMIFACE = 1
+NUMIFACE = 10
 NUMVPN = 1
 IFACESCRIPT = 'ifacetraffic.py'
 VPNSCRIPT = 'vpntraffic.py'
 PASSWORD = ''
 PASSWORDCHECK = True
 CLEARLOGS = False
+if "-c" in str(sys.argv):
+    CLEARLOGS = True
+if ("-h" in str(sys.argv)) or ("--help" in str(sys.argv).lower()):
+    print """RUNSCRIPTS()
+
+    NAME
+        runscripts - start traffic generating scripts
+
+    SYNOPSIS
+        runscripts [ -ch ]
+
+    DESCRIPTION
+        Runscripts starts 0 to many instances of 2 python scripts designed to simulate a heavy udp network traffic load use case. One python script generates udp network traffic through the F5 vpn tunnel while the second python script generates udp network traffic directly to the physical interface. The intent of this program is to reproduce ENOBUFS error messages (SYSCALL 55) on the F5 vpn tunnel when F5 vpn tunnel is running in DTLS tunnel type mode.
+
+    OPTIONS
+        -c  Clear F5 log files under ~/Library/Logs/F5networks and quit program. Helpful to capture logs that only pertain to the next program run.
+
+        -h
+        --help  Print this help information and exit"""
+    quit()
 
 # Get sudo password for commands requiring elevation rights
 if PASSWORDCHECK:
@@ -37,11 +58,16 @@ def runcommand( command ):
         #stdout=subprocess.PIPE,
         #stderr=subprocess.PIPE,
         )
-
+def exit():
+    command = "echo %s | sudo -S printf 'Stopping script instances...\n' | for pid in $(ps axc|awk '{if ($5==\"python\") print $1}'); do sudo kill -9 $pid; done;" % PASSWORD
+    runcommand( command ) 
+    quit(1)
 # Main
 if CLEARLOGS:
     command = 'rm -f ~/Library/Logs/F5Networks/*.log'
     runcommand( command )
+    print "F5 logs cleared"
+    exit()
 print 'Ramping up connections...'
 if NUMVPN:
     command = 'python %s' % VPNSCRIPT
@@ -53,6 +79,4 @@ if NUMIFACE:
 	runcommand( command )
 	time.sleep(0.5)
 response = raw_input("Running %s instance of %s and %s instances of %s\nPress Enter to stop..." % (NUMVPN, VPNSCRIPT, NUMIFACE, IFACESCRIPT))
-
-command = "echo %s | sudo -S printf 'Stopping script instances...\n' | for pid in $(ps axc|awk '{if ($5==\"python\") print $1}'); do sudo kill -9 $pid; done;" % PASSWORD
-runcommand( command )
+exit()
